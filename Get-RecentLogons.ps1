dd176b8ff18e670369f79d0f5191d9fdde2fecdf
dadevel <dadevel@disroot.org>
2024-01-19 14:56:50 +0100

    update logon collector script

--- Get-RecentLogons.ps1
+++ Get-RecentLogons.ps1
@@ -1,15 +1,20 @@
 function Get-RecentLogons {
+    # usage: Get-RecentLogons | ConvertTo-Json | Out-File -FilePath \\tsclient\share\logons-$env:computername.json -Encoding utf8
+     param (
+        [Int] $Days = 30
+    )
     $system = Get-WmiObject -Class Win32_ComputerSystem
     if ($system.PartOfDomain) {
         $computer = $system.Name + '.' + $system.Domain
     } else {
         $computer = $system.Name
     }
-    # fetch logon events from last 30 days
-    Get-WinEvent -LogName security -MaxEvents 16384 -FilterXPath 'Event[System[EventID=4624 and TimeCreated[timediff(@SystemTime) < 2592000000]]]' | %{
+    $delta = 1000 * 60 * 60 * 24 * $Days
+    Get-WinEvent -LogName security -FilterXPath "Event[System[EventID=4624 and TimeCreated[timediff(@SystemTime) < ${delta}]]]" | %{
         @{
             'timestamp'=$_.TimeCreated.ToUniversalTime().ToString('yyyy-MM-ddThh:mm:ss.000000Z');
             'computerfqdn'=$computer;
+            'sourceip'=$_.Properties[18].Value;
             'userdomain'=$_.Properties[6].Value;
             'username'=$_.Properties[5].Value;
             'usersid'=$_.Properties[4].Value.Value;
@@ -20,5 +25,5 @@ function Get-RecentLogons {
     } | ?{
         # keep only real users
         $_['usersid'] -match '^S-1-5-21-'
-    } | ConvertTo-Json
+    }
 }
